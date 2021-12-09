384e9740be283900641103f866b010bbfcbb86f5
Maytham Fahmi <maythamfahmi@nextbix.dk>
2021-12-09 15:14:09 +0100

    improvement 3

--- src/main/java/dk/nextbix/storage/StorageApplication.java
+++ src/main/java/dk/nextbix/storage/StorageApplication.java
@@ -1,12 +1,13 @@
 package dk.nextbix.storage;
 
+import com.azure.spring.autoconfigure.storage.StorageHealthConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {StorageHealthConfiguration.class})
 public class StorageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StorageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class, args);
+    }
 }
